1c621c90bfbf35741b3712bc006ede9b19e12c30
s2-fidan <sude2.fidan@live.uwe.ac.uk>
2023-03-10 17:36:04 +0000

    population of heap

--- heap.py
+++ heap.py
@@ -12,8 +12,64 @@ class MorseHeap:
                     '5','4','','3','','¿','','2','&','','+','','','','','1','6','=','/','','','','(','','7','','','','8','','9','0',
                     '','','','','','','','','','','','','?','_','','','','','”','','','.','','','','','','','','','’','','','-','','','','','','','','',';','!','',')','','','','¡','',',','','','','',':','','','','','','','',
                     '','','','','','','','','','$'] 
+        
+
+    '''def decode_bt(self,code):
+        decoded = ""
+        index = 0
+        #split the words: / means space between words in morsecode
+        for word in code.split("/"):
+            for letter in word.split():
+                for c in letter:
+                    #dot means left child, dash means right child
+                    if c == ".":
+                        index = (index * 2) + 1
+                        print(index)
+                    elif c == "-":
+                        index = (index * 2) + 2
+            decoded += self.heap[index]
+            decoded += " "
+        
+        return decoded #Displaying the message'''
+    
+    def decode_bt(self,morseCode):
+        message = "" # Here we start off with an empty string as our output.
+        value = 1 # We set the value to 1 as the first position in the string.
+        i = 0
+
+        while i in range(len(morseCode)): # Looping over morse code.
+            if i < (len(morseCode) - 1): # Forloops start with 0 so we need to - 1 to length.
+                if morseCode[i] == "." : # First check
+                    value = 2*value # Update variable of value to go down the 'left tree'.
+                elif morseCode[i] == "-": # Second check
+                    value = 2*value + 1 # Update variable of value to go down the 'right tree'.
+                elif morseCode[i] == "/": # Third check defines a space
+                    message += " "
+                elif morseCode[i] == " ":
+                    message += self.heap[value - 1]
+                    value = 1
+            else: # closes loop on last morse.
+                if morseCode[i] == ".": 
+                    value = 2*value 
+                elif morseCode[i] == "-": 
+                    value = 2*value + 1
+                message += self.heap[value - 1] # Add character with the string index to our message.
+                value = 1 # Reset the value to 1 for next loop.
+            i += 1
+        return message
 
 
+
+
+morse = MorseHeap()
+morseCode = "- .... .. ... / .. ... / .- / - . ... -" #THIS IS A TEST
+
+print(morse.decode_bt(morseCode))
+
+
+
+
+"""
 def decode_bt(message):
 
     message = message.split(' ') #Splits the morse code word into morse code
@@ -67,4 +123,4 @@ def decode_ham(message):
             self.print_tree(node.left, prefix + 'l ')
         if node.right:
             #r means right child of the root
-            self.print_tree(node.right, prefix + 'r ')
+            self.print_tree(node.right, prefix + 'r ')"""
